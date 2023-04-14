1558ba8be502d71eac4d496544fa399e138191eb
afigmatrix <122856539+afigmatrix@users.noreply.github.com>
2023-04-14 20:36:10 +0400

    added project

---
+++ Lesson14042023/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Lesson14042023
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gv_Customer = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxCustomerName = new System.Windows.Forms.TextBox();
+            this.tbxCustomerAge = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxCitizenship = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.tbxDeleteName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gv_Customer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.gv_Customer);
+            this.panel1.Location = new System.Drawing.Point(12, 398);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1244, 352);
+            this.panel1.TabIndex = 0;
+            // 
+            // gv_Customer
+            // 
+            this.gv_Customer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gv_Customer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gv_Customer.Location = new System.Drawing.Point(0, 0);
+            this.gv_Customer.Name = "gv_Customer";
+            this.gv_Customer.RowTemplate.Height = 25;
+            this.gv_Customer.Size = new System.Drawing.Size(1244, 352);
+            this.gv_Customer.TabIndex = 0;
+            this.gv_Customer.SelectionChanged += new System.EventHandler(this.gv_Customer_SelectionChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Customer Name";
+            // 
+            // tbxCustomerName
+            // 
+            this.tbxCustomerName.Location = new System.Drawing.Point(29, 132);
+            this.tbxCustomerName.Name = "tbxCustomerName";
+            this.tbxCustomerName.Size = new System.Drawing.Size(216, 23);
+            this.tbxCustomerName.TabIndex = 2;
+            // 
+            // tbxCustomerAge
+            // 
+            this.tbxCustomerAge.Location = new System.Drawing.Point(29, 210);
+            this.tbxCustomerAge.Name = "tbxCustomerAge";
+            this.tbxCustomerAge.Size = new System.Drawing.Size(216, 23);
+            this.tbxCustomerAge.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Customer Age";
+            // 
+            // tbxCitizenship
+            // 
+            this.tbxCitizenship.Location = new System.Drawing.Point(29, 292);
+            this.tbxCitizenship.Name = "tbxCitizenship";
+            this.tbxCitizenship.Size = new System.Drawing.Size(216, 23);
+            this.tbxCitizenship.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Citizenship";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(30, 339);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(215, 36);
+            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // tbxDeleteName
+            // 
+            this.tbxDeleteName.Location = new System.Drawing.Point(921, 132);
+            this.tbxDeleteName.Name = "tbxDeleteName";
+            this.tbxDeleteName.Size = new System.Drawing.Size(262, 23);
+            this.tbxDeleteName.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(921, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Delete";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(921, 180);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(215, 36);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1268, 748);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbxDeleteName);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.tbxCitizenship);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxCustomerAge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxCustomerName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gv_Customer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView gv_Customer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxCustomerName;
+        private System.Windows.Forms.TextBox tbxCustomerAge;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxCitizenship;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox tbxDeleteName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ Lesson14042023/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson14042023
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Server=localhost;Database=Matrix132;Integrated Security=true;";
+        int deleteUserId = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            this.gv_Customer.SelectionChanged -= new System.EventHandler(this.gv_Customer_SelectionChanged);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand selectQuery = new SqlCommand("select * from Customer", conn);
+                DataTable customerList = new DataTable();
+                conn.Open();
+                var sqlResult = selectQuery.ExecuteReader();
+                customerList.Load(sqlResult);
+                conn.Close();
+                gv_Customer.DataSource = customerList;
+            }
+            this.gv_Customer.SelectionChanged += new System.EventHandler(this.gv_Customer_SelectionChanged);
+
+
+
+            #region FileOperations
+
+
+            //FileStream fs = File.Create(@"D:\DevGroup\Matrix132\Projects\test.txt");
+            //fs.Close();
+            //if (File.Exists(@"D:\DevGroup\Matrix132\Projects\test.txt"))
+            //{
+            //    //StreamWriter writer = new StreamWriter(@"D:\DevGroup\Matrix132\Projects\test.txt");
+            //    //writer.WriteLine("Hi");
+            //    //writer.Close();
+            //    //StreamReader sr = new(@"D:\DevGroup\Matrix132\Projects\test.txt");
+            //    //var result = sr.ReadToEnd();
+            //}
+            //else
+            //{
+            //    FileStream fs = File.Create(@"D:\DevGroup\Matrix132\Projects\test.txt");
+            //    fs.Close();
+            //}
+            #endregion
+            #region DirectoryOperations
+            //try
+            //{
+            //    throw new ArgumentNullException("Argument is not valid");
+            //}
+            //catch (Exception ex)
+            //{
+            //    string exePath = Directory.GetCurrentDirectory();
+            //    string fullPath = Path.Combine(exePath, "Log");
+            //    string filePath = Path.Combine(fullPath, "Log14042023.txt");
+            //    //OccuredExcep
+            //    Directory.CreateDirectory(fullPath);
+            //    DirectoryInfo di = new DirectoryInfo(filePath);
+            //    FileStream fs = File.Create(filePath);
+            //    StreamWriter sr = new StreamWriter(fs);
+            //    sr.WriteLine(ex.Message);
+            //    sr.Close();
+            //    fs.Close();
+            //}
+            #endregion
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string Name = tbxCustomerName.Text;
+            string Age = tbxCustomerAge.Text;
+            string Citizenship = tbxCitizenship.Text;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand insertQuery = new SqlCommand($"insert into Customer values(@pName,@pAge,@pCS) ", conn);
+
+                insertQuery.Parameters.AddWithValue("@pName", Name);
+                insertQuery.Parameters.AddWithValue("@pAge", Age);
+                insertQuery.Parameters.AddWithValue("@pCS", Citizenship);
+                conn.Open();
+                _ = insertQuery.ExecuteNonQuery();
+                SqlCommand selectQuery = new SqlCommand("select * from Customer", conn);
+                DataTable customerList = new DataTable();
+                var sqlResult = selectQuery.ExecuteReader();
+                customerList.Load(sqlResult);
+                conn.Close();
+                gv_Customer.DataSource = null;
+                gv_Customer.DataSource = customerList;
+            }
+
+
+        }
+
+        private void gv_Customer_SelectionChanged(object sender, EventArgs e)
+        {
+            int rowIndex = gv_Customer.CurrentCell.RowIndex;
+            var custmerInfos = gv_Customer.Rows[rowIndex];
+            string customerName = custmerInfos.Cells["CustomerName"].Value.ToString();
+            deleteUserId = (int)custmerInfos.Cells["ID"].Value;
+            tbxDeleteName.Text = customerName;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (deleteUserId > 0)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    SqlCommand deleteQuery = new SqlCommand($"delete  from Customer where ID = {deleteUserId}", conn);
+                    conn.Open();
+                   _=deleteQuery.ExecuteNonQuery();
+
+                    SqlCommand selectQuery = new SqlCommand("select * from Customer", conn);
+                    DataTable customerList = new DataTable();
+                    var sqlResult = selectQuery.ExecuteReader();
+                    customerList.Load(sqlResult);
+                    conn.Close();
+                    gv_Customer.DataSource = customerList;
+                }
+            }
+        }
+    }
+}
